68d854479b93a3e4c67b162e6a6012a4b1d7e7b8
小沼智則 <omatomemail0718@gmail.com>
2021-03-24 14:22:14 +0900

    メッセージ送信機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :room_users
-         has_many :messages
+  has_many :room_users
   has_many :rooms, through: :room_users
+  has_many :messages
   validates :name, presence: true
 end
